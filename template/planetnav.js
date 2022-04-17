107725d564204e80fe433d88afb72edc88775986
jamaaldev <kabarojamaal@gmail.com>
2022-04-17 07:54:05 +0100

    added:mobile,tablet

---
+++ template/planetnav.js
@@ -0,0 +1,26 @@
+// header navigation
+
+document.getElementById("planetnav").insertAdjacentHTML(
+  "afterend",
+  `
+
+      <nav  class="planet__nav">
+        <ul class="planet__ul flex" id="planet__ul">
+          <li>
+            <a href="./index.html">Moon</a>
+          </li>
+          <li>
+            <a href="./destination-moon.html">Mars</a>
+          </li>
+          <li>
+            <a href="">Europa</a>
+          </li>
+          <li>
+            <a href="">Titan</a>
+          </li>
+        </ul>
+      </nav>
+
+
+`,
+);
