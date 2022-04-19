e74f5b69867538068dc64b43ca3b4fa50b621b95
jamaaldev <kabarojamaal@gmail.com>
2022-04-19 22:31:26 +0100

    changed

--- template/navigation.js
+++ template/navigation.js
@@ -16,10 +16,10 @@ document.getElementById("nav").insertAdjacentHTML(
             <a href="./destination-moon.html"><span aria-hidden="true">01</span>Destination</a>
           </li>
           <li>
-            <a href=""><span aria-hidden="true">02</span>Crew</a>
+            <a href="./crew-commander.html"><span aria-hidden="true">02</span>Crew</a>
           </li>
           <li>
-            <a href=""><span aria-hidden="true">03</span>Technology</a>
+            <a href="./technology-spaceport.html"><span aria-hidden="true">03</span>Technology</a>
           </li>
         </ul>
       </nav>
